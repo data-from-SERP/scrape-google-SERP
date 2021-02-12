24d63ccb3ffd51b2977b115d15eba632caf0f8eb
Giuseppe Ferrari <35732497+ibeppo993@users.noreply.github.com>
2021-02-12 14:50:31 +0100

    Update parse_html.py

--- parse_html.py
+++ parse_html.py
@@ -8,22 +8,16 @@ from parserp import organic_results
 from parserp import related_searches
 from parserp import inline_shopping
 
-
 #variabili
 html_file = 'output_html'
 html_file_error = 'output_html_error'
 output_files = 'output_data'
-file_related_searches = 'related_searches.csv'
-file_organic_results = 'organic_results.csv'
-file_inline_shopping = 'inline_shopping.csv'
-file_ads = 'ads.csv'
 
 #creazione cartelle
 if not os.path.exists(output_files):
     os.makedirs(output_files)
 if not os.path.exists(html_file_error):
     os.makedirs(html_file_error)
-
 
 files = [file for file in os.listdir(html_file) if '.html' in file]
 for file in files:
