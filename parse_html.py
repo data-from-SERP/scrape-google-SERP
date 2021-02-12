56e0017d761e4b4ead22eedf4ecaeb1f94b387f5
Giuseppe Ferrari <35732497+ibeppo993@users.noreply.github.com>
2021-02-12 17:04:52 +0100

    Update parse_html.py

--- parse_html.py
+++ parse_html.py
@@ -19,12 +19,26 @@ if not os.path.exists(output_files):
 if not os.path.exists(html_file_error):
     os.makedirs(html_file_error)
 
+# #test single file
+# file = '20210212-135545-bombole+a+gas+per+barbecue.html'
+# print(file)
+# #filename = file
+# soup = soup_from_file.get_soup_from_file(file)
+# organic_results.get_organic_results(soup)
+# #related_searches.get_related_searches(soup)
+# #inline_shopping.get_inline_shopping(soup)
+# print('finito file')
+
+#per la lista di file
+counter_file = 1
 files = [file for file in os.listdir(html_file) if '.html' in file]
 for file in files:
     print(file)
     #filename = file
     soup = soup_from_file.get_soup_from_file(file)
     organic_results.get_organic_results(soup)
-    related_searches.get_related_searches(soup)
+    #related_searches.get_related_searches(soup)
     #inline_shopping.get_inline_shopping(soup)
+    print(f'------------ file eseguiti : {counter_file}')
+    counter_file += 1
     print('finito file')
