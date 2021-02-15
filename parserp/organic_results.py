30d72f02652b47e9aa53cc9ce73a99ca7647f908
Giuseppe Ferrari <35732497+ibeppo993@users.noreply.github.com>
2021-02-15 14:52:08 +0100

    Update organic_results.py

--- parserp/organic_results.py
+++ parserp/organic_results.py
@@ -29,30 +29,27 @@ def get_organic_results(soup):
         html_organic_results.find('div',class_='mnr-c').decompose()
 
     # estrazione dati
-    try:
-        organic_results = html_organic_results.find_all('div',class_='g')
-        #print(organic_results)
-        for organic_result in organic_results:
-            keyword = soup.find('title').text.strip().split('-')[0]
-            #print(keyword)
-            div_obj['Keyword'].append(keyword)
-            #posizione + 1
-            div_obj['Position'].append(position)
-            #print(position)
-            position +=1
-            title = organic_result.find('h3').text.strip()
-            #print(title)
-            div_obj['Titles'].append(title)
-            link = organic_result.find('a').attrs['href']
-            #print(link)
-            div_obj['Links'].append(link)
+    organic_results = html_organic_results.find_all('div',class_='g')
+    #print(organic_results)
+    for organic_result in organic_results:
+        keyword = soup.find('title').text.strip().split('-')[0]
+        #print(keyword)
+        div_obj['Keyword'].append(keyword)
+        #posizione + 1
+        div_obj['Position'].append(position)
+        #print(position)
+        position +=1
+        title = organic_result.find('h3').text.strip()
+        #print(title)
+        div_obj['Titles'].append(title)
+        link = organic_result.find('a').attrs['href']
+        #print(link)
+        div_obj['Links'].append(link)
 
-        #print(div_obj)
-        div_obj_df = pd.DataFrame(div_obj, index=None)
-        now = datetime.now()
-        dt_string = now.strftime("%Y%m%d-%H")
-        #print(dt_string)
-        div_obj_df.to_csv(f'{output_files}/{dt_string}-{file_organic_results}', mode='a', header=False, index=False, encoding='UTF-8', sep=';')
-        print('---- organic_results')
-    except:
-        print('---------------------------------pass2')
+    #print(div_obj)
+    div_obj_df = pd.DataFrame(div_obj, index=None)
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d-%H")
+    #print(dt_string)
+    div_obj_df.to_csv(f'{output_files}/{dt_string}-{file_organic_results}', mode='a', header=False, index=False, encoding='UTF-8', sep=';')
+    print('---- organic_results')
