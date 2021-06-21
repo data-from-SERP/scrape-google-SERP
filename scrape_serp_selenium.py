d5724a7c0ab3669b11ba4f690e2dc786d5b31ed0
Giuseppe Ferrari <35732497+ibeppo993@users.noreply.github.com>
2021-06-21 15:13:19 +0200

    add search volume

--- scrape_serp_selenium.py
+++ scrape_serp_selenium.py
@@ -5,12 +5,14 @@ from pandas import DataFrame
 from datetime import datetime
 from datetime import timedelta
 from selenium import webdriver
+import logging
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from scrape_prepare import create_db_and_folder
 from dotenv import load_dotenv
 load_dotenv()
 
+#logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 create_db_and_folder()
 
 #
@@ -38,7 +40,7 @@ def select_proxy():
     #print(type(data['PROXY'].iat[0]))
     global proxy
     proxy = (data['PROXY'].iat[0])
-    print(f'---------------------Request IP is {proxy}')
+    #print(f'---------------------Request IP is {proxy}')
     timestr_now = str(datetime.now())
     #print(timestr_now)
     #global timestr
@@ -52,7 +54,7 @@ def select_proxy():
     conn.close()
 
 def select_keyword():
-    print('-------------------------')
+    #print('-------------------------')
     conn = sqlite3.connect(db_name_keyword)
     c = conn.cursor()
     data = pd.read_sql_query("SELECT KEYWORDS FROM KEYWORDS_LIST WHERE SUM <> 2 AND CHECKING = 0 LIMIT 1;",conn)
@@ -81,15 +83,16 @@ option.add_experimental_option("excludeSwitches", ["enable-automation"])
 option.add_experimental_option('useAutomationExtension', False)
 option.add_argument('--disable-blink-features=AutomationControlled')
 #Proxy
-#option.add_argument(f'proxy-server={proxy}')
+option.add_argument(f'proxy-server={proxy}')
 #headless
-#option.add_argument("--headless")
+option.add_argument("--headless")
 #incognito
 option.add_argument('--incognito')
 #
 #creazione e apertura browsers
 driver = webdriver.Chrome('./chromedriver',options=option)
 driver.maximize_window()
+time.sleep(1)
 
 '''
 try:
@@ -98,19 +101,22 @@ try:
 except:
     print('Accettazione Cookie non richiesta')
 '''
-num_random_process = random.randint(15,20)
+num_random_process = random.randint(4,8)
 for _ in range(num_random_process):
     select_keyword()
     print(f'new_keyword ------- {new_keyword}')
     #accettazione cookie
     new_url = f'{domain_search}{new_keyword}&oq={new_keyword}&hl={hl}&gl={gl}&uule={uule}&sourceid=chrome&ie=UTF-8'
+    #logging.debug(new_url)
+
     #print(new_url)
+    #time.sleep(1)
     driver.get(new_url)
     current_url = driver.current_url
     captcha_url = 'sorry/index?continue'
-    print(current_url)
+    #print(current_url)
     if captcha_url in current_url:
-        print('captcha trovato in URL')
+        #print('captcha trovato in URL')
         timestr = time.strftime('%Y%m%d-%H%M%S')
         with open(test_proxy, 'a') as f:
             f.write(f"{proxy};{timestr};Richiesta Captcha;{new_keyword};\n")
@@ -122,7 +128,7 @@ for _ in range(num_random_process):
         conn.close()
         conn = sqlite3.connect(db_name_proxy)
         c = conn.cursor()
-        print('---------------------Proxy da posticipare '+proxy)
+        #print('---------------------Proxy da posticipare '+proxy)
         postpone_time = str(datetime.now() + timedelta(hours=2))
         timestr_postpone = datetime.fromisoformat(postpone_time).timestamp()
         c.execute("Update PROXY_LIST set TIME = ? where PROXY = ?",(timestr_postpone,proxy))
@@ -132,18 +138,18 @@ for _ in range(num_random_process):
         exit()
     else:
         print('---------------procediamo')
-    
+    '''
     try:
         iframe = driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
         driver.find_element_by_xpath('//*[@id="introAgreeButton"]').click()
         driver.switch_to.default_content()
-        print('Accettazione Cookie andata a buon fine')
+        #print('Accettazione Cookie andata a buon fine')
     except:
         print('Accettazione Cookie non richiesta')
-    
+    '''
     timestr = time.strftime('%Y%m%d-%H%M%S')
     filename = f'{output_html}/{timestr}-{new_keyword}.html'
-    print(filename)
+    #print(filename)
     soup = BeautifulSoup(driver.page_source,'html.parser')
     #print(soup)
     #driver.execute_script("window.scrollTo(0, Y)")
@@ -154,16 +160,16 @@ for _ in range(num_random_process):
     #print('Scroll di pagina effettuato')
     if os.path.exists(filename):
         statinfo = os.stat(filename)
-        print(statinfo.st_size)
+        #print(statinfo.st_size)
         if statinfo.st_size > 70000:
 
             #primo controllo
             if soup.find('div',id='introAgreeButton') is None:
                 print('YYYYYYYYYY-no introAgreeButton')
             else:
-                print('XXXXXXXXXX-introAgreeButton')
+                #print('XXXXXXXXXX-introAgreeButton')
                 os.remove(filename)
-                print('file HTML eliminato correttamente')
+                #print('file HTML eliminato correttamente')
                 conn = sqlite3.connect(db_name_keyword)
                 c = conn.cursor()
                 c.execute("Update KEYWORDS_LIST set CHECKING = 0 where KEYWORDS = ?",(keyword,))
@@ -171,7 +177,7 @@ for _ in range(num_random_process):
                 conn.close()
                 with open(test_proxy, 'a') as f:
                     f.write(f"{proxy};{timestr};Richiesta Fallita;{new_keyword};\n")
-                    print('Log scritto correttamente')
+                    #print('Log scritto correttamente')
                 conn = sqlite3.connect(db_name_proxy)
                 c = conn.cursor()
                 #print(type(data['PROXY'].iat[0]))
@@ -191,7 +197,7 @@ for _ in range(num_random_process):
             else:
                 print('XXXXXXXXXX-sub-frame-error-details')
                 os.remove(filename)
-                print('file HTML eliminato correttamente')
+                #print('file HTML eliminato correttamente')
                 conn = sqlite3.connect(db_name_keyword)
                 c = conn.cursor()
                 c.execute("Update KEYWORDS_LIST set CHECKING = 0 where KEYWORDS = ?",(keyword,))
@@ -199,10 +205,10 @@ for _ in range(num_random_process):
                 conn.close()
                 with open(test_proxy, 'a') as f:
                     f.write(f"{proxy};{timestr};Richiesta Fallita;{new_keyword};\n")
-                    print('Log scritto correttamente')
+                    #print('Log scritto correttamente')
                 conn = sqlite3.connect(db_name_proxy)
                 c = conn.cursor()
-                print('---------------------Proxy da posticipare '+proxy)
+                #print('---------------------Proxy da posticipare '+proxy)
                 postpone_time = str(datetime.now() + timedelta(minutes=5))
                 timestr_postpone = datetime.fromisoformat(postpone_time).timestamp()
                 c.execute("Update PROXY_LIST set TIME = ? where PROXY = ?",(timestr_postpone,proxy))
@@ -212,24 +218,24 @@ for _ in range(num_random_process):
                 driver.close()
                 exit()
 
-            print('YYYYYYYYYY-no captcha')
+            #print('YYYYYYYYYY-no captcha')
             conn = sqlite3.connect(db_name_keyword)
             c = conn.cursor()
             c.execute("Update KEYWORDS_LIST set SUM = 2 where KEYWORDS = ?",(keyword,))
             conn.commit()
             num = random.randint(1,5)
-            print("Record Updated successfully ")
+            #print("Record Updated successfully ")
             c.close()
             with open(test_proxy, 'a') as f:
                 f.write(f"{proxy};{timestr};Esecuzione Corretta;{new_keyword}\n")
-                print('file scritto correttamente')
+                #print('file scritto correttamente')
             driver.delete_all_cookies()
 
 
         else:
-            print('file minore di 70 KB')
+            #print('file minore di 70 KB')
             os.remove(filename)
-            print('file HTML eliminato correttamente')
+            #print('file HTML eliminato correttamente')
             conn = sqlite3.connect(db_name_keyword)
             c = conn.cursor()
             c.execute("Update KEYWORDS_LIST set CHECKING = 0 where KEYWORDS = ?",(keyword,))
@@ -237,10 +243,10 @@ for _ in range(num_random_process):
             conn.close()
             with open(test_proxy, 'a') as f:
                 f.write(f"{proxy};{timestr};Richiesta Captcha;{new_keyword};\n")
-                print('file scritto correttamente')
+                #print('file scritto correttamente')
             conn = sqlite3.connect(db_name_proxy)
             c = conn.cursor()
-            print('---------------------Proxy da posticipare '+proxy)
+            #print('---------------------Proxy da posticipare '+proxy)
             postpone_time = str(datetime.now() + timedelta(minutes=50))
             timestr_postpone = datetime.fromisoformat(postpone_time).timestamp()
             c.execute("Update PROXY_LIST set TIME = ? where PROXY = ?",(timestr_postpone,proxy))
@@ -253,7 +259,7 @@ for _ in range(num_random_process):
     else:
         print('File non esiste')
 
-print('Keywords scansionata con successo')
+#print('Keywords scansionata con successo')
 driver.delete_all_cookies()
 driver.close()
 exit()
